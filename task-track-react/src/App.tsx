7ecc71004f2d34ce05193230519e7fa74f103bf3
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2018-11-05 21:42:56 +0200

    added UI KIT

--- task-track-react/src/App.tsx
+++ task-track-react/src/App.tsx
@@ -4,17 +4,30 @@ import { Route, Switch } from 'react-router';
 import Home from './containers/home/Home';
 import Tasks from './containers/tasks/Tasks';
 import { Link } from 'react-router-dom';
+import NewTask from './containers/new-task/NewTask';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Link to="/">Home</Link>
-        <Link to="/tasks">Tasks</Link>
-        <Switch>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/tasks" component={Tasks}></Route>
-        </Switch>
+        <nav className="uk-navbar-container uk-margin" uk-navbar="mode: click">
+          <div className="uk-navbar-center">
+            <ul className="uk-navbar-nav">
+              <li className="uk-active"><Link to="/">Home</Link></li>
+              <li><Link to="/tasks">Tasks</Link></li>
+              <li><Link to="/new">New</Link></li>
+            </ul>
+
+          </div>
+        </nav>
+
+        <div className="uk-container">
+          <Switch>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/tasks" component={Tasks}></Route>
+            <Route path="/new" component={NewTask}></Route>
+          </Switch>
+        </div>
       </div>
     );
   }
---
+++ task-track-react/src/containers/new-task/NewTask.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface NewTaskProps {
+}
+
+export default class NewTask extends React.Component<NewTaskProps, any> {
+  public render() {
+    return (
+      <div>
+        New task
+      </div>
+    );
+  }
+}
