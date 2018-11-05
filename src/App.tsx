b65a88905d2a395799ba58115cf96f28f29ba291
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2018-11-05 14:34:03 +0200

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router';
+import Home from './containers/home/Home';
+import Tasks from './containers/tasks/Tasks';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        test
+        <Link to="/">Home</Link>
+        <Link to="/tasks">Tasks</Link>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/tasks" component={Tasks}></Route>
+        </Switch>
       </div>
     );
   }
---
+++ src/containers/tasks/Tasks.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface TasksProps {
+}
+
+export default class Tasks extends React.Component<TasksProps, any> {
+  public render() {
+    return (
+      <div>
+        Tasks
+      </div>
+    );
+  }
+}
