f06cf5b44bb7ff4cfc8b8dda986617c66edd3384
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2018-12-29 22:29:04 +0200

    added backend

---
+++ task-track/src/main/java/com/github/evgdim/tasktrack/security/UserLogin.java
@@ -0,0 +1,9 @@
+package com.github.evgdim.tasktrack.security;
+
+import lombok.Data;
+
+@Data
+public class UserLogin {
+	private String username;
+	private String password;
+}
---
+++ task-track/src/main/java/com/github/evgdim/tasktrack/security/UserToken.java
@@ -0,0 +1,8 @@
+package com.github.evgdim.tasktrack.security;
+
+import lombok.Data;
+
+@Data
+public class UserToken {
+	private final String token;
+}
